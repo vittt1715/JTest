b263cdb453c85b4b4735a97156fcb905b0b63b35
Val <olefir.val@gmail.com>
2017-08-07 19:49:12 +0300

    update

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World, from all DevOps!!!!");
+        System.out.println("Hello, World, from all DevOps!!!");
+        System.out.println("Glad to see you all!!!");
     }
 
 }
